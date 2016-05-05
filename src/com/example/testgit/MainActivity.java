fb9283906971c8e08b803a1485cd35c25fb54ed4
烈 <@Jack-Home>
2016-05-05 19:23:06 +0800

    加入几个button

--- src/com/example/testgit/MainActivity.java
+++ src/com/example/testgit/MainActivity.java
@@ -9,16 +9,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.os.Build;
 
 public class MainActivity extends Activity {
-
+	Button btnProduct;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        btnProduct = (Button)findViewById(R.id.button1);
+        
+        btnProduct.setText("testgit");
     }
 
 
